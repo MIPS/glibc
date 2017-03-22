81e1d8634f677e85d0f6bfdfd515831f75bf73da
Zack Weinberg <zackw@panix.com>
2017-03-22 10:55:38 -0400

    Remove __need_IOV_MAX and __need_FOPEN_MAX.
    __need_FOPEN_MAX wasn't being used anywhere.  __need_IOV_MAX was more
    complicated; the basic deal is that sys/uio.h wants to define a
    constant named UIO_MAXIOV and bits/xopen_lim.h wants to define a
    constant named IOV_MAX, with the same meaning.  For no apparent reason
    this was being handled via bits/stdio_lim.h -- stdio.h is NOT supposed
    to define IOV_MAX -- and some mess in Makerules.  Also, bits/uio.h on
    Linux was being used as a dumping ground for extension functions, and
    there's no longer any good reason for the definition of struct iovec
    itself to be system-dependent.
    
    So now we have bits/uio_lim.h, which defines __IOV_MAX.
    bits/xopen_lim.h and sys/uio.h use that to define their respective
    constants.  We also now have bits/types/struct_iovec.h, which is the
    one true definition of struct iovec, used by sys/uio.h and
    bits/fcntl-linux.h (another dumping ground for extension functions
    which I am not tackling in this patch series), and sys/uio_ext.h,
    which is the official Proper Home for extensions to sys/uio.h.  And
    stdio_lim.h doesn't define IOV_MAX at all.
    
    	* bits/uio_lim.h, sysdeps/unix/sysv/linux/bits/uio_lim.h
    	* misc/bits/types/struct_iovec.h, include/bits/types/struct_iovec.h
    	* sysdeps/generic/sys/uio_ext.h, sysdeps/unix/sysv/linux/sys/uio_ext.h:
    	New file.
    	* bits/uio.h, sysdeps/unix/sysv/linux/bits/uio.h: Delete file.
    
    	* include/bits/xopen_lim.h: Use bits/uio_lim.h to get the value
    	for IOV_MAX.
    	* misc/Makefile: Install sys/uio_ext.h, bits/uio_lim.h, and
    	bits/types/struct_iovec.h. Don't install bits/uio.h.
    	* misc/sys/uio.h: Use bits/types/struct_iovec.h to get the
    	definition of struct iovec, and bits/uio_lim.h to get the value
    	for UIO_MAXIOV.  Under __USE_GNU, include sys/uio_ext.h.
    	* sysdeps/unix/sysv/linux/bits/fcntl-linux.h:
    	Use bits/types/struct_iovec.h to get the definition of struct
    	iovec.  Use __ssize_t, not ssize_t, in function prototypes.
    
    	* stdio-common/stdio_lim.h.in: Remove logic for __need_FOPEN_MAX
    	and __need_IOV_MAX.  Don't define IOV_MAX at all.
    	* Makerules (stdio_lim.h): Remove logic for setting IOV_MAX.

--- sysdeps/unix/sysv/linux/sys/uio_ext.h
+++ sysdeps/unix/sysv/linux/sys/uio_ext.h
@@ -15,38 +15,10 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#if !defined _SYS_UIO_H && !defined _FCNTL_H
-# error "Never include <bits/uio.h> directly; use <sys/uio.h> instead."
-#endif
+#ifndef _SYS_UIO_EXT_H
+#define _SYS_UIO_EXT_H 1
 
-#ifndef _BITS_UIO_H
-#define _BITS_UIO_H	1
-
-#include <sys/types.h>
-
-
-/* We should normally use the Linux kernel header file to define this
-   type and macros but this calls for trouble because of the header
-   includes other kernel headers.  */
-
-/* Size of object which can be written atomically.
-
-   This macro has different values in different kernel versions.  The
-   latest versions of the kernel use 1024 and this is good choice.  Since
-   the C library implementation of readv/writev is able to emulate the
-   functionality even if the currently running kernel does not support
-   this large value the readv/writev call will not fail because of this.  */
-#define UIO_MAXIOV	1024
-
-
-#include <bits/types/struct_iovec.h>
-
-#endif
-
-
-#ifdef __USE_GNU
-# if defined _SYS_UIO_H && !defined _BITS_UIO_H_FOR_SYS_UIO_H
-#  define _BITS_UIO_H_FOR_SYS_UIO_H 1
+#include <sys/uio.h>
 
 __BEGIN_DECLS
 
@@ -68,5 +40,4 @@ extern ssize_t process_vm_writev (pid_t __pid, const struct iovec *__lvec,
 
 __END_DECLS
 
-# endif
-#endif
+#endif /* sys/uio_ext.h */
