3dacf35f0a1e249bd04f1c62ed99687421154936
Zack Weinberg <zackw@panix.com>
2019-03-17 09:50:36 -0400

    Swap sys/poll.h with poll.h.
    Similarly to (sys/)syslog.h, poll.h is the header standardized by
    POSIX, but we had all of its contents in sys/, for historical reasons.
    This patch exchanges the contents of the two headers, and adds
    multiple-include guards to all of poll.h’s bits headers.
    
    	* io/poll.h: Exchange contents with...
    	* io/sys/poll.h: ...this file.  Adjust guard macros.
    
    	* include/poll.h: Exchange contents with...
    	* include/sys/poll.h: ...this file.  Adjust guard macros.
    
    	* bits/poll.h, io/bits/poll2.h
    	* sysdeps/unix/sysv/linux/bits/poll.h
    	* sysdeps/unix/sysv/linux/m68k/bits/poll.h
    	* sysdeps/unix/sysv/linux/mips/bits/poll.h
    	* sysdeps/unix/sysv/linux/sparc/bits/poll.h:
            Allow inclusion by poll.h, not sys/poll.h.  Add multiple-
            include guards where not already present.
    
    	* scripts/check-obsolete-constructs.py (HEADER_ALLOWED_INCLUDES):
            Update.

--- include/poll.h
+++ include/poll.h
@@ -1,1 +1,11 @@
-#include <include/sys/poll.h>
+#ifndef _POLL_H
+# include <io/poll.h>
+
+#ifndef _ISOMAC
+extern int __poll (struct pollfd *__fds, unsigned long int __nfds,
+		   int __timeout);
+libc_hidden_proto (__poll)
+libc_hidden_proto (ppoll)
+#endif
+
+#endif
