c7d1a41c54e746fb256aec82f3c09b977b4379d1
Florian Weimer <fweimer@redhat.com>
2020-02-10 16:21:32 +0100

    s390: Remove backchain-based fallback from backtrace
    This backtrace variant is used when _Unwind_Backtrace is not found in
    libgcc_s.  It still depends on _Unwind_GetIP from libgcc_s, so it does
    not work if loading libgcc_s fails.  Therefore, as long as libgcc_s
    provides a definition of _Unwind_Backtrace, the fallback is not useful.
    
    After this change, the implementation is the same on s390 and s390x
    and can be shared.

--- sysdeps/s390/backtrace.c
+++ sysdeps/s390/backtrace.c
@@ -24,36 +24,6 @@
 #include <stdlib.h>
 #include <unwind.h>
 
-/* This is a global variable set at program start time.  It marks the
-   highest used stack address.  */
-extern void *__libc_stack_end;
-
-
-/* This is the stack layout we see for every non-leaf function.
-           size                    offset
-    %r15 ->    +------------------+
-             4 | back chain       |  0
-             4 | end of stack     |  4
-             8 | glue             |  8
-             8 | scratch          | 16
-            40 | save area r6-r15 | 24
-            16 | save area f4,f6  | 64
-            16 | empty            | 80
-               +------------------+
-   r14 in the save area holds the return address.
-*/
-
-struct layout
-{
-  int back_chain;
-  int end_of_stack;
-  int glue[2];
-  int scratch[2];
-  int save_grps[10];
-  int save_fp[4];
-  int empty[2];
-};
-
 struct trace_arg
 {
   void **array;
@@ -76,32 +46,6 @@ init (void)
   unwind_getip = __libc_dlsym (handle, "_Unwind_GetIP");
   if (unwind_getip == NULL)
     unwind_backtrace = NULL;
-}
-
-static int
-__backchain_backtrace (void **array, int size)
-{
-  /* We assume that all the code is generated with frame pointers set.  */
-  struct layout *stack;
-  int cnt = 0;
-
-  __asm__ ("LR  %0,%%r15" : "=d" (stack) );
-  /* We skip the call to this function, it makes no sense to record it.  */
-  stack = (struct layout *) stack->back_chain;
-  while (cnt < size)
-    {
-      if (stack == NULL || (void *) stack > __libc_stack_end)
-	/* This means the address is out of range.  Note that for the
-	   toplevel we see a frame pointer with value NULL which clearly is
-	   out of range.  */
-	break;
-
-      array[cnt++] = (void *) (stack->save_grps[8] & 0x7fffffff);
-
-      stack = (struct layout *) stack->back_chain;
-    }
-
-  return cnt;
 }
 #else
 # define unwind_backtrace _Unwind_Backtrace
@@ -136,7 +80,7 @@ __backtrace (void **array, int size)
   __libc_once (once, init);
 
   if (unwind_backtrace == NULL)
-    return __backchain_backtrace (array, size);
+    return 0;
 #endif
 
   unwind_backtrace (backtrace_helper, &arg);
