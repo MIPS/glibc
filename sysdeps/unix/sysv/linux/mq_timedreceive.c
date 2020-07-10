22fa4975e6ca9f65172e977b372fcd74a56983cd
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-07-10 10:41:23 -0300

    linux: Simplify mq_timedreceive
    With arch-syscall.h it can now assumes the existance of either
    __NR_mq_timedreceive or __NR_mq_timedreceive_time64.  The 32-bit
    time_t support is now only build for !__ASSUME_TIME64_SYSCALLS.
    
    The internal 64-bit time_t names is also adjusted to use the
    other y2038 symbol names.
    
    Checked on x86_64-linux-gnu and i686-linux-gnu (on 5.4 and on 4.15
    kernel).

--- sysdeps/unix/sysv/linux/mq_timedreceive.c
+++ sysdeps/unix/sysv/linux/mq_timedreceive.c
@@ -22,19 +22,17 @@
 /* Receive the oldest from highest priority messages in message queue
    MQDES, stop waiting if ABS_TIMEOUT expires.  */
 ssize_t
-__mq_timedreceive_time64 (mqd_t mqdes, char *__restrict msg_ptr, size_t msg_len,
-                          unsigned int *__restrict msg_prio,
-                          const struct __timespec64 *__restrict abs_timeout)
+__mq_timedreceive64 (mqd_t mqdes, char *__restrict msg_ptr, size_t msg_len,
+                     unsigned int *__restrict msg_prio,
+                     const struct __timespec64 *__restrict abs_timeout)
 {
-#ifdef __ASSUME_TIME64_SYSCALLS
-# ifndef __NR_mq_timedreceive_time64
-#  define __NR_mq_timedreceive_time64 __NR_mq_timedreceive
-# endif
-  return SYSCALL_CANCEL (mq_timedreceive_time64, mqdes, msg_ptr, msg_len,
-                         msg_prio, abs_timeout);
-#else
+#ifndef __NR_mq_timedreceive_time64
+# define __NR_mq_timedreceive_time64 __NR_mq_timedreceive
+#endif
   int ret = SYSCALL_CANCEL (mq_timedreceive_time64, mqdes, msg_ptr, msg_len,
-                            msg_prio, abs_timeout);
+			    msg_prio, abs_timeout);
+
+#ifndef __ASSUME_TIME64_SYSCALLS
   if (ret == 0 || errno != ENOSYS)
     return ret;
 
@@ -50,13 +48,15 @@ __mq_timedreceive_time64 (mqd_t mqdes, char *__restrict msg_ptr, size_t msg_len,
       ts32 = valid_timespec64_to_timespec (*abs_timeout);
     }
 
-  return SYSCALL_CANCEL (mq_timedreceive, mqdes, msg_ptr, msg_len, msg_prio,
-                         abs_timeout != NULL ? &ts32 : NULL);
+  ret = SYSCALL_CANCEL (mq_timedreceive, mqdes, msg_ptr, msg_len, msg_prio,
+			abs_timeout != NULL ? &ts32 : NULL);
 #endif
+
+  return ret;
 }
 
 #if __TIMESIZE != 64
-librt_hidden_def (__mq_timedreceive_time64)
+librt_hidden_def (__mq_timedreceive64)
 
 ssize_t
 __mq_timedreceive (mqd_t mqdes, char *__restrict msg_ptr, size_t msg_len,
@@ -67,8 +67,8 @@ __mq_timedreceive (mqd_t mqdes, char *__restrict msg_ptr, size_t msg_len,
   if (abs_timeout != NULL)
     ts64 = valid_timespec_to_timespec64 (*abs_timeout);
 
-  return __mq_timedreceive_time64 (mqdes, msg_ptr, msg_len, msg_prio,
-                                   abs_timeout != NULL ? &ts64 : NULL);
+  return __mq_timedreceive64 (mqdes, msg_ptr, msg_len, msg_prio,
+			      abs_timeout != NULL ? &ts64 : NULL);
 }
 #endif
 
