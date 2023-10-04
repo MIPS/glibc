0addcc788c06309484902df9502ce21ab230ad48
Chao-ying Fu <cfu@wavecomp.com>
2023-10-04 15:06:54 -0700

    Support the big-endian mode.

--- sysdeps/riscv/bits/endianness.h
+++ sysdeps/riscv/bits/endianness.h
@@ -5,7 +5,11 @@
 # error "Never use <bits/endianness.h> directly; include <endian.h> instead."
 #endif
 
+#ifdef __riscv_big_endian
+#define __BYTE_ORDER __BIG_ENDIAN
+#else
 /* RISC-V is little-endian.  */
 #define __BYTE_ORDER __LITTLE_ENDIAN
+#endif
 
 #endif /* bits/endianness.h */
